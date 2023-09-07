55312aaf5b6804d23c53b18cb1ff4b7123e8fd09
Daniel Jeffery <danielajeffery@gmail.com>
2023-09-06 20:24:23 -0700

    Collision detection

--- rollup.config.js
+++ rollup.config.js
@@ -32,6 +32,5 @@ export default {
         commonjs({
            defaultIsModuleExports: true
         }),
-        ]
-        //terser(),]
+        terser(),]
 };
--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,4 +1,15 @@
-import {emit, GameObject, getPointer, init, on, pointerPressed, Scene, Vector} from "kontra";
+import {
+    collides,
+    emit,
+    GameObject,
+    init,
+    on,
+    Pool,
+    Quadtree,
+    Scene,
+    Sprite,
+    Vector
+} from "kontra";
 import {CPlayer} from "../vendor/player-small";
 import BeatBeat from "../vendor/beat-beat-js";
 import {player} from "../actors/player";
@@ -6,6 +17,7 @@ import {sky} from "../actors/sky";
 import {gameSong} from "../music";
 import {Crosshair} from "../actors/crosshair";
 
+let { context } = init();
 //declare const testSong: any;
 
 export const gameScene = Scene({
@@ -48,7 +60,63 @@ export const gameScene = Scene({
         ])
     },
     update() {
+        pool.update()
         this.objects = this.objects?.filter((o) => (o as GameObject).isAlive())
         this.objects?.forEach((o) => (o as GameObject).update())
+
+        let quadtree = Quadtree();
+        // @ts-ignore
+        quadtree.add(player, pool.getAliveObjects())
+        // @ts-ignore
+        const touching = quadtree.get(player).filter((f) => collides(player, f))
+
+    },
+    render() {
+        this.objects?.forEach((o) => (o as GameObject).render())
+        pool.render()
     }
 });
+
+
+let pool = Pool({
+    // @ts-ignore
+    create: Sprite,
+});
+
+pool.get({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    color: 'red',
+    opacity: 0,
+    ttl: 0
+});
+
+on("explode", (position: Vector) => {
+    for(let f = 0; f < 10; f++) {
+        const pos = (f / 10) * 2 * Math.PI
+        const radius = 5
+
+        pool.get({
+            position,
+            width: 10,
+            height: 10,
+            color: 'yellow',
+            ttl: 240,
+            dx: radius * Math.cos(pos) * 0.25,
+            dy: radius * Math.sin(pos) * 0.25,
+            render: () => {
+                context.fillStyle = 'red'
+                context.beginPath();
+                context.arc(0, 0, 5, 0, 2 * Math.PI);
+                context.fill();
+
+                context.fillStyle = 'yellow'
+                context.beginPath();
+                context.arc(0, 0, 3, 0, 2 * Math.PI);
+                context.fill();
+            }
+        });
+    }
+})
